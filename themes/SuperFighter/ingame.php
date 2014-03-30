3026818260d1ad53477b692f30b6bb1678ed9815
Dees Oomens <d.oomens@hotmail.nl>
2014-03-30 15:53:06 +0200

    Started with second theme

---
+++ themes/SuperFighter/ingame.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title><?php //echo $info['titel']." | ".$info['link']->titel; ?></title>
+        <base href="<?php echo $info['base']; ?>" />
+        <script src="files/js/jquery.min.js"></script>
+        <script src="files/js/jquery-ui.min.js"></script>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="themes/SuperFighter/css/style.css">
+        <link rel="stylesheet" href="themes/SuperFighter/css/menu.css">
+    </head>
+    <body>
+        <header class="main-header">
+            <div class="left-header">
+                <h2 class="title">osFighter</h2>
+            </div>
+            <div class="right-header">
+                content
+            </div>
+        </header>
+        <div class="main-menu">
+
+        </div>
+        <div class="user-menu">
+
+        </div>
+    </body>
+</html>
