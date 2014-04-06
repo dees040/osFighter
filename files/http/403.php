028ae52dd88a289cb485e0df000d0b4b2cb7cf43
Dees Oomens <d.oomens@hotmail.nl>
2014-04-06 19:46:04 +0200

    Error reports

--- files/http/403.php
+++ files/http/403.php
@@ -1,2 +1,3 @@
 <?php
-    echo "403";
+    echo "<h1>404 | Permissions denied.</h1>";
+    echo "Sorry, but you don 't have the permissions to view this page.";
--- files/http/404.php
+++ files/http/404.php
@@ -1,2 +1,3 @@
 <?php
-    echo "404";
+    echo "<h1>404 | Page not found.</h1>";
+    echo "Sorry, but your request (".$_SERVER['REQUEST_URI'].") could not be in our system.";
