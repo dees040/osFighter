9c4c58f5113768a4ad6b277c2b67fc098549df0b
Dees Oomens <d.oomens@hotmail.nl>
2014-04-10 17:40:29 +0200

    Update file-system page

--- core/admin.php
+++ core/admin.php
@@ -2,52 +2,29 @@
 
 class admin
 {
-    function admin() {
+    var $errorArray;
+    var $reportArray;
 
+    function admin() {
+        $this->errorArray  = array();
+        $this->reportArray = array();
     }
 
+    /**
+     * Form checker for form file-system create page
+     * @param $title
+     * @param $link
+     * @param $file
+     * @param $category
+     * @return array returns array with error's, returns empty array when 0 errors
+     */
     function fileSystemCreateForm($title, $link, $file, $category) {
         global $database;
-        $errorArray = array();
-        $reportArray = array();
 
-        // Error checking for title
-        if (!$title) {
-            $errorArray[] = " - Please fill in a title.";
-        } else {
-            $items = array(':title' => $title);
-            $query = $database->select("SELECT * FROM ".TBL_PAGES." WHERE title = :title", $items);
+        $this->checkFileSystemForm($title, $link, $file, $category, true);
 
-            if ($query->rowCount()) $reportArray[] = " - Title already exists.";
-        }
-
-        if (!$link) {
-            $errorArray[] = " - Please fill in a link.";
-        } else {
-            $items = array(':link' => $link);
-            $query = $database->select("SELECT id FROM ".TBL_PAGES." WHERE link = :link", $items);
-
-            //if ($query->rowCount()) $errorArray[] = " - Link already exists.";
-        }
-
-        if (!$file) {
-            $errorArray[] = " - Please fill in a file.";
-        } else {
-            $items = array(':file' => $file);
-            $query = $database->select("SELECT id FROM ".TBL_PAGES." WHERE file = :file", $items);
-
-            if ($query->rowCount()) {
-                $reportArray[] = " - Another page already use this file.";
-            } else {
-                if (!file_exists("files/ingame/".$category."/".$file)) {
-                    $reportArray[] = " - Because the file not exists yet, i created it for you.";
-                    touch("files/ingame/".$category."/".$file);
-                }
-            }
-        }
-
-        if (!empty($errorArray)) {
-            return $errorArray;
+        if (!empty($this->errorArray)) {
+            return $this->errorArray;
         }
 
         $items = array(':title' => $title, ':link' => $link, ':file' => "ingame/".$category."/".$file);
@@ -60,7 +37,95 @@ class admin
         $items = array(':pid' => $pid, ':link' => $link, ':menu' => $category);
         $database->insert("INSERT INTO ".TBL_MENUS." SET pid = :pid, link = :link, menu = :menu", $items);
 
-        return $reportArray;
+        return $this->reportArray;
+    }
+
+    /**
+     * Form checker for form file-system edit page
+     * @param $title
+     * @param $link
+     * @param $file
+     * @param $category
+     * @return array returns array with error's, returns empty array when 0 errors
+     */
+    function fileSystemEditForm($title, $link, $file, $category) {
+        global $database;
+
+        $this->checkFileSystemForm($title, $link, $file, $category, false);
+
+        if (!empty($this->errorArray)) {
+            return $this->errorArray;
+        }
+
+        $items = array(':title' => $title, ':link' => $link, ':file' => "ingame/".$category."/".$file, 'id' => $_SESSION['get-page-id']);
+        $database->insert("UPDATE ".TBL_PAGES." set title = :title, link = :link, file = :file WHERE id = :id", $items);
+
+        $items = array(':pid' => $_SESSION['get-page-id'], ':link' => $link, ':menu' => $category);
+        $database->insert("INSERT INTO ".TBL_MENUS." SET pid = :pid, link = :link, menu = :menu", $items);
+
+        return $this->reportArray;
+    }
+
+    /**
+     * Check form values
+     * @param $title
+     * @param $link
+     * @param $file
+     * @param $category
+     * @param $createPage returns array with error's, returns empty array when 0 errors
+     */
+    function checkFileSystemForm($title, $link, $file, $category, $createPage) {
+        global $database;
+
+        // Error checking for title
+        if (!$title) {
+            $this->errorArray[] = " - Please fill in a title.";
+        } else {
+            if ($createPage) {
+                $items = array(':title' => $title);
+                $query = $database->select("SELECT id FROM ".TBL_PAGES." WHERE title = :title", $items);
+            } else {
+                $items = array(':title' => $title, ':id' => $_SESSION['get-page-id']);
+                $query = $database->select("SELECT id FROM ".TBL_PAGES." WHERE title = :title AND id != :id", $items);
+            }
+
+            if ($query->rowCount()) $this->reportArray[] = " - Title already exists.";
+        }
+
+        if (!$link) {
+            $this->errorArray[] = " - Please fill in a link.";
+        } else {
+            if ($createPage) {
+                $items = array(':link' => $link);
+                $query = $database->select("SELECT id FROM ".TBL_PAGES." WHERE link = :link", $items);
+            } else {
+                $items = array(':link' => $link, ':id' => $_SESSION['get-page-id']);
+                $query = $database->select("SELECT * FROM ".TBL_PAGES." WHERE link = :link AND id != :id", $items);
+            }
+
+            if ($query->rowCount()) $this->errorArray[] = " - Link already exists.";
+        }
+
+        if (!$file) {
+            $this->errorArray[] = " - Please fill in a file.";
+        } else {
+            if ($createPage) {
+                $items = array(':file' => $file);
+                $query = $database->select("SELECT id FROM ".TBL_PAGES." WHERE file = :file", $items);
+            } else {
+                $items = array(':file' => $file, ':id' => $_SESSION['get-page-id']);
+                $query = $database->select("SELECT * FROM ".TBL_PAGES." WHERE file = :file AND id != :id", $items);
+            }
+
+            if ($query->rowCount()) {
+                $this->reportArray[] = " - Another page already use this file.";
+            } else {
+                if (!file_exists("files/ingame/".$category."/".$file)) {
+                    $this->reportArray[] = " - Because the file not exists yet, i created it for you.";
+                    touch("files/ingame/".$category."/".$file);
+                }
+            }
+        }
     }
 }
 
--- files/ingame/admin/files.php
+++ files/ingame/admin/files.php
@@ -40,7 +40,7 @@
                         Link:
                     </td>
                     <td>
-                        <?= URL; ?>/ <input type="text" name="link" size="30" class="link">
+                        <?= $info['base']; ?> <input type="text" name="link" size="30" class="link">
                     </td>
                 </tr>
                 <tr>
@@ -79,6 +79,91 @@
 
     <!-- file-sytem page tab1: Edit -->
     <div id="tab3">
+        <?php
+            $pages = $database->select("SELECT * FROM ".TBL_PAGES." ORDER BY title ASC");
 
+            if (isset($_POST['submit-get-page'])) {
+                $items = array(':id' => $_POST['page']);
+                $query = $database->select("SELECT * FROM ".TBL_PAGES." WHERE id = :id", $items);
+                $page  = $query->fetchObject();
+                $_SESSION['get-page-id'] = $_POST['submit-get-page'];
+            }
+
+            if (isset($_POST['submit-edit-page'])) {
+                $retval = $admin->fileSystemEditForm($_POST['title'], $_POST['link'], $_POST['file'], $_POST['category']);
+
+                if (empty($retval)) {
+                    echo "<strong>New page created</strong>";
+                } else {
+                    foreach($retval as $error) {
+                        echo $error."<br>";
+                    }
+                }
+            }
+        ?>
+        <form method="post">
+            <select name="page">
+            <?php
+                foreach($pages->fetchAll() as $item) {
+                    echo "<option value='".$item['id']."'>".$item['title']."</option>";
+                }
+            ?>
+            </select>
+            <input type="submit" value="Get page" name="submit-get-page">
+        </form>
+        <?php if (isset($_POST['submit-get-page'])) { ?>
+
+            <form method="post">
+                <table>
+                    <tr>
+                        <td>
+                            Title:
+                        </td>
+                        <td>
+                            <input type="text" name="title" maxlength="100" size="30" class="title" value="<?= $page->title; ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Link:
+                        </td>
+                        <td>
+                            <?= $info['base']; ?> <input type="text" name="link" size="30" class="link" value="<?= $page->link; ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            File:
+                        </td>
+                        <td>
+                            <input type="text" name="file" size="30" class="file" placeholder="The filename" value="<?= $page->file; ?>">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Category:
+                        </td>
+                        <td>
+                            <select name="category" class="category">
+                                <option value="personal">Personal</option>
+                                <option value="call-credits">Call Credits</option>
+                                <option value="family">Family</option>
+                                <option value="extra">Extra</option>
+                                <option value="crime">Crime</option>
+                                <option value="locations">Locations</option>
+                                <option value="casino">Casino</option>
+                                <option value="admin">Admin</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <input type="submit" value="Edit page" style="float: right" name="submit-edit-page">
+                        </td>
+                    </tr>
+                </table>
+            </form>
+
+        <?php } ?>
     </div>
 </div>
