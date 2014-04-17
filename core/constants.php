bf698ece91d4e7a66d2edbaac9e69471cc848f4e
Dees Oomens <d.oomens@hotmail.nl>
2014-04-17 13:23:30 +0200

    removed some settings

--- core/constants.php
+++ core/constants.php
@@ -7,9 +7,9 @@
  */
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "deb65120n5_os");
-define("DB_PASS", "osFighter1234!");
-define("DB_NAME", "deb65120n5_osf");
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 /**
  * Database Table Constants - these constants
@@ -26,6 +26,7 @@ define("TBL_MENUS", "menus");
 define("TBL_INFO", "users_info");
 define("TBL_TIME", "users_time");
 define("TBL_GROUPS", "groups");
+define("TBL_CRIMES", "crimes");
 
 
 /**
