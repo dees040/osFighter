ed911e31c9c28eeeeffdb4bee199be2c8c41ea9f
dees040 <d.oomens@hotmail.nl>
2014-11-20 08:58:49 +0100

    Update db

--- core/constants.php
+++ core/constants.php
@@ -7,9 +7,9 @@
  */
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "deb65120n6_osf");
-define("DB_PASS", "dees1234!");
-define("DB_NAME", "deb65120n6_os");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "osf");
 
 /**
  * Database Table Constants - these constants
