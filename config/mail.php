9c1216720b342c08ef7decf4488a8912673dd7cb
dees040 <d.oomens@hotmail.nl>
2016-11-15 15:33:09 +0100

    Added email name and address

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'hello@osfighter.com',
+        'name' => 'osFighter',
     ],
 
     /*
